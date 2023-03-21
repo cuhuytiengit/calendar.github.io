6aca90b60b72b7f20930a67bbd8e840afc376ad7
cuhuytiengit <cuhuytien2000github@gmail.com>
2023-03-21 16:30:23 +0700

    change

--- calendar.js
+++ calendar.js
@@ -1,35 +1,48 @@
-const test = document.getElementById("view-event");
-const getHours = document.getElementById("hours");
-const getMinutes = document.getElementById("minutes");
-const getSeconds = document.getElementById("seconds");
+var test = document.getElementById("view-event");
+var getHours = document.getElementById("hours");
+var getMinutes = document.getElementById("minutes");
+var getSeconds = document.getElementById("seconds");
 
-const time = new Date();
+var time = new Date();
 hours = time.getHours();
 minutes = time.getMinutes();
 seconds = time.getSeconds();
 milliseconds = time.getMilliseconds();
-minutes = minutes < 10 ? "0" + minutes : minutes;
-seconds = seconds < 10 ? "0" + seconds : seconds;
-setInterval(setMinutes, 60000);
-setInterval(setSecond, 1000);
+setInterval(setMinutes, 1000);
+setInterval(setSecond, 700);
 
 getHours.innerHTML = hours;
 getMinutes.innerHTML = minutes;
 function setMinutes() {
-  if (minutes > 0 && minutes <= 59) {
-    minutes++;
-    getMinutes.innerHTML = minutes;
-  } else if (minutes > 59) {
-    seconds === 00;
+  console.log("test");
+  console.log(seconds);
+  console.log(minutes);
+  console.log(getSeconds.value);
+  if (seconds == 0 && seconds > 58) {
+    getMinutes.innerHTML = minutes++;
+  } else if (seconds == 0) {
     minutes++;
     getMinutes.innerHTML = minutes;
   }
 }
 
 function setSecond() {
-  if (seconds > 0 && seconds <= 59) {
-    seconds++;
-    getSeconds.innerHTML = seconds;
+  if (seconds >= -1 && seconds <= 58) {
+    if (seconds < 9) {
+      seconds++;
+      getSeconds.innerHTML = "0" + seconds;
+    } else {
+      seconds++;
+      getSeconds.innerHTML = seconds;
+    }
+    console.log("test time oout 35");
+  } else if (seconds > 58) {
+    console.log("test time oout");
+    setTimeout(function test() {
+      seconds = -1;
+      getSeconds.innerHTML = seconds;
+      console.log("test time oout1");
+    });
   }
 }
 
